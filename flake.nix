9daa45aa68c5f0a068174c989a7ee8d6de00df65
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-09-01 14:14:21 +0000

    Initial state

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "My Ubuntu Home Manager config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      homeConfigurations.myuser = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          {
+            home.username = "myuser";
+            home.homeDirectory = "/home/myuser";
+
+            # Basic example: install some packages
+            home.packages = with pkgs; [
+              neovim
+              htop
+              cowsay
+            ];
+
+            programs.zsh.enable = true;
+            programs.git.enable = true;
+
+            # Always recommended
+            home.stateVersion = "24.05"; # Adjust to current HM release
+          }
+        ];
+      };
+    };
+}
