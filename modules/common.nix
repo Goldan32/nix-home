d1e1f7a01373fb27eff4219270632f0f8daeb027
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-09-23 16:31:16 +0200

    Introduce moduls in home-config

---
+++ modules/common.nix
@@ -0,0 +1,92 @@
+{ config, lib, pkgs, jotter, system, dotfiles, ... }: 
+let
+  git-matr = import ../modules/matr.nix { inherit pkgs; };
+
+  configDirs = builtins.attrNames (builtins.readDir "${dotfiles}/.config");
+
+  blacklist = [ "Code" ];
+  filteredConfigDirs = lib.filter (dir: !(builtins.elem dir blacklist)) configDirs;
+
+  configAttrs = builtins.listToAttrs (map (dir: {
+    name = ".config/${dir}";
+    value = {
+      source = "${dotfiles}/.config/${dir}";
+      recursive = true;
+    };
+  }) filteredConfigDirs);
+in
+{
+  home.packages = with pkgs; [
+    git-matr
+    hostname
+    acpi
+
+    git
+    zsh
+    gnumake
+    htop
+    gcc
+    tree
+    gnutar
+    cmake
+    ninja
+    gettext
+    pkg-config
+    unzip
+    sqlite
+    dunst
+    fd
+
+    neovim
+    ripgrep
+    stylua
+    bash-language-server
+    typescript-language-server
+    prettier
+    pyright
+    shellcheck
+
+    cargo
+    bat
+    jotter.packages.${system}.default
+    cargo-generate
+
+    nodejs
+    typescript
+    nodenv
+
+    zoxide
+    jq
+  ];
+
+  #
+  # Dotfiles
+  #
+  home.file = lib.mkMerge [
+    configAttrs
+    {
+      ".zshrc".source = "${dotfiles}/.zshrc";
+      ".zsh/machines".source = "${dotfiles}/.zsh/machines";
+      ".zsh/patches".source = "${dotfiles}/.zsh/patches";
+      ".zsh/scripts".source = "${dotfiles}/.zsh/scripts";
+      ".local/scripts".source = "${dotfiles}/.local/scripts";
+      ".local/start-page".source = "${dotfiles}/.local/start-page";
+
+      # Beg for dark mode
+      ".config/gtk-3.0/settings.ini".text = ''
+        [Settings]
+        gtk-error-bell=false
+        gtk-application-prefer-dark-theme=1
+      '';
+      ".config/gtk-4.0/settings.ini".text = ''
+        [Settings]
+        gtk-error-bell=false
+        gtk-application-prefer-dark-theme=1
+      '';
+    }
+  ];
+
+  home.activation.batCacheBuild = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ${pkgs.bat}/bin/bat cache --build
+  '';
+}
---
+++ modules/de.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, system, ... }:
+{
+  home.packages = with pkgs; [
+    wofi
+    firefox
+    roboto-mono
+    wezterm
+    pamixer
+  ];
+
+  #
+  # Dunst service
+  #
+  systemd.user.services.dunst = {
+    Unit = {
+      Description = "Dunst Notification Daemon";
+      After = "default.target";
+    };
+    Install.WantedBy = ["default.target"];
+    Service = {
+      Type = "exec";
+      ExecStart = "${pkgs.dunst}/bin/dunst";
+      # Restart = "always";
+      # RestartSec = 5;
+    };
+  };
+}
+
