2162d678f944bc9f1413e41968dd2961abc115e5
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-09-25 19:06:22 +0200

    Cleanup and matr bump

--- modules/matr.nix
+++ modules/matr.nix
@@ -5,8 +5,8 @@ pkgs.stdenv.mkDerivation {
 
   src = pkgs.fetchgit {
     url = "https://github.com/Goldan32/git-matr.git";
-    rev = "303b957bc9f7614f7d92db51d465148f8c57d78f";
-    sha256 = "sha256-CCu24T/pMIc5EKZBhmPLC7ZFZbZ1sToaGPJOKo0JK3g=";
+    rev = "fb3629fab1495e46ba81354be26142176b95db17";
+    sha256 = "sha256-YffJGeD/zeLacNI37WgqaOR72HkGLdDkpUYDbB5A+cY=";
   };
 
   installPhase = ''
