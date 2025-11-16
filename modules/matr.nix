e835093ee0dfd93c5c5d46b619b63b692953bc56
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-11-16 09:31:29 +0100

    Bump matr

--- modules/matr.nix
+++ modules/matr.nix
@@ -1,12 +1,12 @@
 { pkgs }:
 pkgs.stdenv.mkDerivation {
   pname = "git-matr";
-  version = "0.1.0";
+  version = "0.2.0";
 
   src = pkgs.fetchgit {
     url = "https://github.com/Goldan32/git-matr.git";
-    rev = "fb3629fab1495e46ba81354be26142176b95db17";
-    sha256 = "sha256-YffJGeD/zeLacNI37WgqaOR72HkGLdDkpUYDbB5A+cY=";
+    rev = "15bc40aba900afb5fa78dbf6ec8c2330aa25aa6f";
+    sha256 = "sha256-t3ORVt5gy/5XI3cHPmHi/tXT4H6+Jwb4XtO64UQpi6Y=";
   };
 
   installPhase = ''
