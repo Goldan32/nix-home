86d0bbd20a408ae1cb916e3715f4d9a6ee7475a8
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2026-06-20 22:03:50 +0200

    Small nix-home update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-SRC_DIR="$(dirname ${BASH_SOURCE[0]})"
-nix-shell -p home-manager --run "home-manager switch --flake ${SRC_DIR}#goldan"
+SRC_DIR="$(dirname "$(realpath ${BASH_SOURCE[0]})")"
+nix-shell -p home-manager --run "home-manager switch --flake ${SRC_DIR}#goldan --override-input dotfiles ${SRC_DIR}/dotfiles"
